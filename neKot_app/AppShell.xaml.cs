85104d11b8fca1da9d9c9851459ecaae64183994
Cat Cay <col1nkl@yandex.ru>
2020-10-30 21:57:25 +0700

    Добавьте файлы проекта.

---
+++ neKot_app/AppShell.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using neKot_app.ViewModels;
+using neKot_app.Views;
+using Xamarin.Forms;
+
+namespace neKot_app
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+    }
+}
---
+++ neKot_app/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using neKot_app.ViewModels;
+
+namespace neKot_app.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
