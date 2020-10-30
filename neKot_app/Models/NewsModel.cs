afaee04b3521a2ff95312c750cfd79457f14f4c4
Ilya Fedorov <ilya.f.s01@mail.ru>
2020-10-30 22:14:31 +0700

    News model added

---
+++ neKot_app/Models/NewsModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace neKot_app.Models
+{
+    public class NewsModel
+    {
+        public string Title { get; set; }
+        public string Avatar { get; set; }
+        public DateTime Date { get; set; }
+        public string  Link { get; set; }
+    }
+}
