5d435e95c52c4a59bfae6a36012057c36206e9e5
Ilya Fedorov <ilya.f.s01@mail.ru>
2020-10-31 00:33:09 +0700

    Achievement search service

---
+++ neKot_app/Services/AchivementsSearch.cs
@@ -0,0 +1,23 @@
+﻿using neKot_app.Models;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using Utf8Json;
+using System.Threading.Tasks;
+
+namespace neKot_app.Services
+{
+    public class AchivementsSearch
+    {
+        private HttpClient html = new HttpClient();
+        private string url = "https://edo.72to.ru/genius/children?number=25&surname_firstname=";
+        
+        public async Task<List<Achivement>> GetAchivementsByName(User user)
+        {
+            string response = (await html.GetAsync(url + user.FirstName + user.LastName)).Content.ToString();
+            List<Achivement> achivements = JsonSerializer.Deserialize<List<Achivement>>(response);
+            return achivements;
+        }
+    }
+}
