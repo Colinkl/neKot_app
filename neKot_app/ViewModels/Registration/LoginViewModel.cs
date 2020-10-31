2bf7152e72a426adef911bf60506a915d0ce7574
Cat Cay <col1nkl@yandex.ru>
2020-10-31 20:27:46 +0700

    AuthPage fix + Login Page fix

--- neKot_app/ViewModels/Registration/LoginViewModel.cs
+++ neKot_app/ViewModels/Registration/LoginViewModel.cs
@@ -3,27 +3,52 @@ using neKot_app.Services;
 using neKot_app.Views;
 using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
 using Xamarin.Forms;
 
 namespace neKot_app.ViewModels
 {
+
     public class LoginViewModel : BaseViewModel
     {
+        private AchivementsSearch FirstachivementsSearch;
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Phone {get; set;}
         public string Password {get; set;}
+        private string errMessage;
+        public string ErrMessage
+        {
+            get => errMessage;
+            set
+            {
+                if (value == errMessage)
+                {
+                    return;
+                }
+                errMessage = value;
+                OnPropertyChanged(nameof(ErrMessage));
+            }
+        }
         public Command LoginCommand { get; }
 
         public LoginViewModel()
         {
             LoginCommand = new Command(OnLoginClicked);
+            FirstachivementsSearch = new AchivementsSearch(DependencyService.Get<HttpClient>());
         }
 
         private async void OnLoginClicked(object obj)
         {
-            DependencyService.Get<UserSaveService>().SaveUser(new User() { FirstName = this.FirstName, LastName = this.LastName});
+            var tempUser = new User(){ FirstName = this.FirstName, LastName = this.LastName } ;
+            var tempList = await FirstachivementsSearch.GetAchivementsByName(tempUser);
+            if (tempList.Count == 0)
+            {
+                ErrMessage = "Пользователь не найден" ;
+                return;
+            }
+            DependencyService.Get<UserSaveService>().SaveUser(tempUser);
             await Shell.Current.GoToAsync("///Profile");
         }
     }
