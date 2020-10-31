1e54a958413b28a9a1f67e891ab2abd8651e9955
Ilya Fedorov <ilya.f.s01@mail.ru>
2020-10-31 12:19:50 +0700

    Cleaninig project

--- neKot_app/AppShell.xaml.cs
+++ neKot_app/AppShell.xaml.cs
@@ -11,8 +11,6 @@ namespace neKot_app
         public AppShell()
         {
             InitializeComponent();
-            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
-            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
         }
 
     }
